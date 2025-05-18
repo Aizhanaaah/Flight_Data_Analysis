967904b8b87cda35353728bb7c4ed1c57c7056dd
Aizhan Amanova <131990972+Aizhanaaah@users.noreply.github.com>
2025-05-18 18:32:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -33,17 +33,18 @@ if response.status_code == 200:
         df = df[df['origin_country'] == 'Hungary']
         df["time_position"] = pd.to_datetime(df["time_position"], unit="s")
         df["last_contact"] = pd.to_datetime(df["last_contact"], unit="s")
+        df = df.dropna(subset=["longitude", "latitude", "icao24", "callsign"])
         df.to_csv(filename, mode="a", header=False, index=False)
         print('Data successfully appended to existing file.')
 else:
     print(f"Failed to retrieve data: {response.status_code}")
 
 
-gdf = gpd.GeoDataFrame(
-    df,
-    geometry=gpd.points_from_xy(df.longitude, df.latitude),
-    crs="EPSG:4326"  
+
+gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.longitude, df.latitude),crs="EPSG:4326"  
 )
+
+
 
 print(gdf.head())
 gdf.plot(marker="o", color="blue", markersize=5, figsize=(10, 6))
@@ -53,13 +54,30 @@ plt.ylabel("Latitude")
 plt.show()
 '''
 '''
-# Initialize map centered at an average location
+
+on_ground_counts = df['on_ground'].value_counts()
+print(on_ground_counts)
+on_ground = on_ground_counts.get(True, 0)
+above_ground = on_ground_counts.get(False, 0)
+labels = ['On Ground', 'In Air']
+sizes = [on_ground, above_ground]
+colors = ['#ff9999', '#66b3ff']
+explode = (0.05, 0)
+plt.figure(figsize=(6, 6))
+plt.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=140,
+        colors=colors, explode=explode, shadow=True)
+plt.title('Aircraft Status: On Ground vs In Air')
+plt.axis('equal') 
+plt.show()
+
+
+
 m = folium.Map(location=[50, 10], zoom_start=4)
 gdf_clean = gdf.dropna(subset=["latitude", "longitude"])
 heat_data = [[row.latitude, row.longitude] for _, row in gdf_clean.iterrows()]
 HeatMap(heat_data).add_to(m)
 
-# Add aircraft markers
+
 for _, row in gdf_clean.iterrows():
     folium.Marker(
         location=[row.latitude, row.longitude],
@@ -67,11 +85,10 @@ for _, row in gdf_clean.iterrows():
         icon=folium.Icon(color="blue", icon="plane", prefix="fa")
     ).add_to(m)
 
-# Save and open the map
+
 m.save("aircraft_map.html")
 print("Map successfully generated without NaN locations!")
 display(m)
-
 
 
 '''def clear_csv_file(filename):
@@ -80,4 +97,3 @@ display(m)
     print(f"All data removed from {filename}.")
 
 clear_csv_file("flight_data.csv")'''
-
